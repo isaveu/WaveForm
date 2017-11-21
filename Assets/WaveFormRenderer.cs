0faf36bd770b19a00b0acbfe204ee30a486499a5
Luis Pulido <nosmirck@Luiss-MacBook-Air.local>
2017-11-20 23:15:27 -0500

    initial commit

---
+++ Assets/WaveFormRenderer.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveFormRenderer : MonoBehaviour
+{
+	public int Resolution;
+	public float Size;
+	public float Speed;
+	public float Frequency;
+	public float Amplitude;
+	public Color WaveColor;
+
+	public AnimationCurve Curve;
+
+	float Spacing;
+
+	LineRenderer Wave;
+	Vector3[] Points;
+	// Use this for initialization
+	void Start()
+	{
+		Spacing = Size / (float)Resolution;
+		Points = new Vector3[Resolution];
+		for (int i = 0; i < Resolution; i++)
+			Points[i] = new Vector3(i * Spacing, 0f, 0f);
+		Wave = GetComponent<LineRenderer>();
+		Wave.positionCount = Resolution;
+		Wave.SetPositions(Points);
+		Wave.startWidth = Wave.endWidth = 0.1f;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		Spacing = Size / (float)Resolution;
+		for (int i = 0; i < Resolution; i++)
+		{
+			Points[i].x = i * Spacing;
+			//Points[i].y = Mathf.Sin(Points[i].x * Frequency - Time.time * Speed) * Amplitude;
+			Points[i].y = Curve.Evaluate(Points[i].x * Frequency - Time.time * Speed) * Amplitude;
+		}
+		Wave.SetPositions(Points);
+	}
+
+	public void OnFrequencyChange(float value)
+	{
+		Frequency = value;
+	}
+	public void OnAmplitudeChange(float value)
+	{
+		Amplitude = value;
+	}
+	public void OnSpeedChange(float value)
+	{
+		Speed = value;
+	}
+	public void OnSizeChange(float value)
+	{
+		Size = value;
+	}
+	public void OnResolutionChange(float value)
+	{
+		Resolution = 2 + (10*(int)value);
+		Debug.Log(Resolution.ToString());
+		Wave.positionCount = Resolution;
+		Points = new Vector3[Resolution];
+		for (int i = 0; i < Resolution; i++)
+			Points[i] = new Vector3();
+	}
+}
---
+++ Assets/WaveRenderer.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveRenderer : MonoBehaviour
+{
+	public float Amplitude = 1f;
+	public float Frequency = 2f;
+	public float lifetime = 5f;
+
+	public float minimumVertexDistance = 0.01f;
+
+	public Vector3 velocity;
+
+	public AnimationCurve curve;
+
+	Vector3 markerPosition;
+
+	LineRenderer line;
+	List<Vector3> points;
+	Queue<float> spawnTimes = new Queue<float>();
+
+	void Awake()
+	{
+		markerPosition = transform.position;
+		line = GetComponent<LineRenderer>();
+		line.useWorldSpace = false;
+		points = new List<Vector3>() { markerPosition };
+		line.positionCount = points.Count;
+		line.SetPositions(points.ToArray());
+	}
+
+	void AddPoint(Vector3 position)
+	{
+		points.Insert(1, position);
+		spawnTimes.Enqueue(Time.time);
+	}
+
+	void RemovePoint()
+	{
+		spawnTimes.Dequeue();
+		points.RemoveAt(points.Count - 1);
+	}
+
+	void Update()
+	{
+		markerPosition = Vector3.up * curve.Evaluate(Time.time * Frequency) * Amplitude;
+
+		while (spawnTimes.Count > 0 && spawnTimes.Peek() + lifetime < Time.time)
+		{
+			RemovePoint();
+		}
+
+		Vector3 diff = -velocity * Time.deltaTime;
+		for (int i = 1; i < points.Count; i++)
+		{
+			points[i] += diff;
+		}
+
+		if (points.Count < 2 || Vector3.Distance(markerPosition, points[1]) > minimumVertexDistance)
+		{
+			AddPoint(markerPosition);
+		}
+
+		points[0] = markerPosition;
+
+		line.positionCount = points.Count;
+		line.SetPositions(points.ToArray());
+	}
+
+
+	public void OnFrequencyChange(float value)
+	{
+		Frequency = value;
+	}
+	public void OnAmplitudeChange(float value)
+	{
+		Amplitude = value;
+	}
+}
+
